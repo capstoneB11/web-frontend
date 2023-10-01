89f44e683ded7e69868661f1e693801e978af90d
ranggarahman <rangga.a.r@mail.ugm.ac.id>
2023-10-01 14:09:36 +0700

    refactor : Login Page

--- src/components/Public.js
+++ src/components/Public.js
@@ -1,24 +1,23 @@
-
+import React from 'react';
+import LoginForm from './auth/login/LoginForm';
 
 const Public = () => {
-    return (
-      <div className="min-h-screen flex items-center justify-center bg-gray-100">
-        <div className="text-center">
-          <div className="mb-8">
-            <img 
-                src="https://picsum.photos/200/300" 
-                alt="Welcome" 
-                className="rounded-lg shadow-md w-full" />
-          </div>
-          <h1 className="text-4xl font-bold text-gray-800 mb-4">Hello, get started!</h1>
-          <p className="text-gray-600 mb-6">Welcome to our awesome platform.</p>
-          <div className="space-x-4">
-            <button className="px-6 py-3 bg-blue-500 text-white rounded hover:bg-blue-700">Register</button>
-            <button className="px-6 py-3 bg-gray-200 text-gray-700 rounded hover:bg-gray-300">Sign In</button>
-          </div>
-        </div>
-      </div>
-    );
-  };
+  return (
+    <div className="min-h-screen flex flex-col-reverse lg:flex-row bg-gray-100">
 
-export default Public
+      {/* Left Side (Image) - Hidden on Small Screens */}
+      <div className="hidden lg:block lg:w-1/2 h-1/3 lg:h-screen bg-cover bg-center relative">
+        <img 
+          src="https://picsum.photos/200/300" 
+          alt="Welcome" 
+          className="w-full h-full object-cover object-center" />
+      </div>
+
+      {/* Right Side (Sign-in Form) */}
+      <LoginForm/>
+
+    </div>
+  );
+};
+
+export default Public;
