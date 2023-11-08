92e0754c896255c63a02397171a1e0e3f2c972e1
ranggarahman <rangga.a.r@mail.ugm.ac.id>
2023-11-08 20:02:23 +0700

    refactor : home page position fix, gradient bg

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -73,43 +73,23 @@ const HomePage = () => {
     );
   } else {
     content = (
-      <div className="p-6 sm:p-10 bg-gradient-to-b from-gradient-1 to-gradient-2">
+      <div className="p-6 sm:p-10">
         <h1 className="text-3xl sm:text-4xl font-medium text-white mt-6 mb-4 ">
           Hello, Peternak Yazid! 🧑🏻‍🌾
         </h1>
+        <p className="text-sm text-white italic text-start">
+          {today}, {weatherData.location.name}, {weatherData.location.region}
+        </p>
         <div className="flex flex-col lg:flex-row mt-4">
           <div className="w-full lg:w-1/2 lg:pr-4 mb-4 lg:mb-0">
-            <div className="flex flex-col bg-white p-10 h-full rounded-2xl shadow-2xl justify-between">
+            <div className="flex flex-col bg-white p-12 h-full rounded-2xl shadow-2xl justify-between">
               <h2 className="text-2xl font-semibold text-center">
                 Hasil Prediksi
               </h2>
-              <p className="text-sm italic text-center">
-                {today}, {weatherData.location.name},{" "}
-                {weatherData.location.region}
-              </p>
-              <div className="flex items-center justify-center mt-2">
-                <img
-                  src={weatherData.current.condition.icon}
-                  alt="Icon"
-                  className="w-10 h-10"
-                />
-                <div className="ml-2 mt-4 text-xl">
-                  <p>Cuaca: {weatherData.current.condition.text}</p>
-                  <p>Suhu: {weatherData.current.temp_c}°C</p>
-                </div>
-              </div>
-              <div className="mt-4 flex flex-col items-center justify-center">
-                <div style={{ width: 400, height: 400 }} className="flex-grow">
-                  <Doughnut data={data} className="scale-75 md:scale-100" />
-                </div>
-                {/* <p className="sm:mt-2 ">Persentase Jumlah Ayam</p> */}
-              </div>
-              {/* <ul className="mt-4 text-center">
-                <li>Ayam hidup: {33}</li>
-                <li>Ayam mati: {25}</li>
-              </ul> */}
+              <Doughnut data={data} className="mt-8 scale-75 md:scale-100" />
             </div>
           </div>
+
           <div className="w-full lg:w-1/2">
             <div className="bg-white p-10 h-full rounded-2xl shadow-2xl flex flex-col justify-between">
               <h2 className="text-2xl font-semibold text-center">
