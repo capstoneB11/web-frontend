75a4beda18cfff874435d75549aad10ef4ec1926
ranggarahman <rangga.a.r@mail.ugm.ac.id>
2023-11-09 22:13:14 +0700

    add : tracker page chart and table

--- src/pages/tracker/TrackerPage.js
+++ src/pages/tracker/TrackerPage.js
@@ -16,35 +16,27 @@ import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import useUserToken from "../../hooks/useUserToken";
 import Card from "../../components/dashboard/Card";
+import Loader from "../../utils/Loader";
+import { useChickenCount } from "../../hooks/useChickenCount";
+import HistoryTable from "../../components/dashboard/HistoryTable";
 
 ChartJS.register(BarElement, CategoryScale, LinearScale, Tooltip, Legend);
 
 const TrackerPage = () => {
+  const userToken = useUserToken();
+
+  const { chickenCount, countLoading } = useChickenCount(userToken);
+
   let content;
 
   const [selectedDate, setSelectedDate] = useState(new Date());
 
-  const userToken = useUserToken();
-
-  // Function to generate random data for the chart
-  const generateRandomData = (days) => {
-    const labels = Array.from({ length: days }, (_, i) => `Day ${i + 1}`);
-    const data = Array.from({ length: days }, () =>
-      Math.floor(Math.random() * 100)
-    );
-    return { labels, data };
-  };
-
-  // Initialize the bar chart data with default values
-  const initialDays = 7; // Default number of days
-  const { labels, data } = generateRandomData(initialDays);
-
   const [barChartData, setBarChartData] = useState({
-    labels,
+    labels: [],
     datasets: [
       {
         label: "Jumlah Ayam",
-        data,
+        data: [],
         backgroundColor: "rgba(255, 151, 26, 0.2)",
         borderColor: "rgba(255, 151, 26, 1)",
         borderWidth: 1,
@@ -60,72 +52,93 @@ const TrackerPage = () => {
     },
   };
 
-  // Function to handle date change in the calendar
   const handleDateChange = (date) => {
     setSelectedDate(date);
 
-    const options = {
-      year: "numeric",
-      month: "long", // Use 'long' for the full month name, or 'short' for the abbreviated name
-      day: "numeric",
-    };
+    const today = new Date(); // Today's date
+    // const selected = new Date(date); // Selected date
 
-    // Format the date in Indonesian style
-    const formattedDate = date.toLocaleDateString("id-ID", options);
+    const filteredData = chickenCount.filter((data) => {
+      const dataDate = new Date(data.timestamp);
+      return dataDate >= date && dataDate <= today;
+    });
 
-    const days = Math.floor((Date.now() - date) / (1000 * 60 * 60 * 24)); // Calculate days
-    const { labels, data } = generateRandomData(days);
+    filteredData.sort((a, b) => new Date(a.timestamp) - new Date(b.timestamp)); // Sort by date
+
+    const labels = filteredData.map((data) => {
+      // Format the label based on the date (you can adjust this based on your preferences)
+      const dataDate = new Date(data.timestamp);
+      return format(dataDate, "dd MMMM yyyy", { locale: id }); // Using date string as an example
+    });
+
+    const data = filteredData.map((data) => data.count);
+
     setBarChartData({
       labels,
       datasets: [
         {
-          label: "Number of Chickens",
+          label: "Jumlah Ayam",
           data,
-          backgroundColor: "rgba(75, 192, 192, 0.2)",
-          borderColor: "rgba(75, 192, 192, 1)",
+          backgroundColor: "rgba(255, 151, 26, 0.2)",
+          borderColor: "rgba(255, 151, 26, 1)",
           borderWidth: 1,
         },
       ],
     });
   };
 
-  content = (
-    <div className="p-6 sm:p-10">
-      <h1 className="text-white text-3xl sm:text-4xl font-bold">Statistik</h1>
-
-      <div className="w-full mb-4 mt-4">
-        <Card className="p-4 flex flex-col items-center justify-center">
-          <h2 className="text-xl sm:text-2xl font-bold font-bold mb-2 mt-4">
-            Grafik Jumlah Ayam
-          </h2>
-          <Bar className="p-16" data={barChartData} options={barChartOptions} />
-        </Card>
+  if (countLoading) {
+    content = (
+      <div className="flex h-screen items-center justify-center">
+        <Loader />
       </div>
+    );
+  } else {
+    content = (
+      <div className="p-6 sm:p-10">
+        <h1 className="text-white text-3xl sm:text-4xl font-bold">Statistik</h1>
 
-      <div className="flex flex-col lg:flex-row mt-4">
-        <div className="w-full lg:w-1/2 lg:pr-4 mb-4 lg:mb-0">
-          <Card className="p-4 flex flex-col items-center justify-center h-full">
-            <h2 className="text-xl sm:text-2xl font-bold mb-2">
-              Pilih Riwayat Prediksi
+        <div className="w-full mb-4 mt-4">
+          <Card className="p-4 flex flex-col items-center justify-center">
+            <h2 className="text-xl sm:text-2xl font-bold font-bold mb-2 mt-4">
+              Grafik Jumlah Ayam
             </h2>
-            <Calendar value={selectedDate} onChange={handleDateChange} />
-            <p>
-              Tanggal terpilih sejak:{" "}
-              {format(selectedDate, "dd MMMM yyyy", { locale: id })}
-            </p>
+            <Bar
+              className="p-16"
+              data={barChartData}
+              options={barChartOptions}
+            />
           </Card>
         </div>
-        <div className="w-full lg:w-1/2">
-          <Card className="p-4 flex flex-col items-center justify-center h-full">
-            <h2 className="text-xl sm:text-2xl font-bold mb-2">
-              Tabel Riwayat Prediksi
-            </h2>
-            <div className="w-full h-full bg-gray-400"></div>
-          </Card>
+
+        <div className="flex flex-col lg:flex-row mt-4">
+          <div className="w-full lg:w-1/2 lg:pr-4 mb-4 lg:mb-0">
+            <Card className="p-4 flex flex-col items-center justify-center h-full">
+              <h2 className="text-xl sm:text-2xl font-bold mb-2">
+                Pilih Riwayat Prediksi
+              </h2>
+              <Calendar value={selectedDate} onChange={handleDateChange} />
+              <p>
+                Tanggal terpilih sejak:{" "}
+                {format(selectedDate, "dd MMMM yyyy", { locale: id })}
+              </p>
+            </Card>
+          </div>
+          <div className="w-full lg:w-1/2">
+            <Card className="p-4 flex flex-col items-center justify-center h-full">
+              <h2 className="text-xl sm:text-2xl font-bold mb-2">
+                Tabel Riwayat Prediksi
+              </h2>
+              <HistoryTable
+                selectedDate={selectedDate}
+                chickenCount={chickenCount}
+              />
+            </Card>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 
   return content;
 };
